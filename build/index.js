0f8f9424f8986753b00049dc513dba72d6a3e13f
manuK1777 <manuelkrapovickas@gmail.com>
2024-08-06 13:07:36 +0200

    Exrercise 3 done. Scores layout. Builded array: current date, joke, joke score

--- build/index.js
+++ build/index.js
@@ -8,8 +8,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-//NIvell 1
-//Exercici 1
+const reportAcudits = [];
+let indexArray = 0;
+let jokeScore = 0;
 // Fetch and display the joke when the page loads
 function fetchJoke() {
     return __awaiter(this, void 0, void 0, function* () {
@@ -29,7 +30,14 @@ function fetchJoke() {
             }
             const jokeData = yield response.json();
             const joke = jokeData.joke;
-            jokeElement.textContent = `"${joke}"`; //joke;
+            const currentTime = new Date().toISOString();
+            reportAcudits.push({
+                joke: joke,
+                score: jokeScore,
+                currentTime: currentTime,
+            });
+            indexArray = reportAcudits.length - 1;
+            jokeElement.textContent = `"${joke}"`;
         }
         catch (error) {
             console.error('Failed to fetch joke:', error);
@@ -39,6 +47,23 @@ function fetchJoke() {
 }
 // Fetch and display the joke when the page loads
 document.addEventListener('DOMContentLoaded', fetchJoke);
+console.log(reportAcudits);
 function nextAcudit() {
     fetchJoke();
+    console.log(reportAcudits);
 }
+//Get the joke score from the user
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.btn-group .btn');
+    buttons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const target = event.target;
+            const buttonValue = target.getAttribute('data-value');
+            jokeScore = buttonValue;
+            reportAcudits[indexArray].score = parseInt(jokeScore);
+            //reset variable
+            jokeScore = 0;
+            console.log(reportAcudits);
+        });
+    });
+});
--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
-//NIvell 1
-//Exercici 1
+
+const reportAcudits: {joke: string; score: number; date: string}[] = [];
+let indexArray = 0;
+let jokeScore = 0;
+
 // Fetch and display the joke when the page loads
 async function fetchJoke(): Promise<void> {
   const jokeElement = document.getElementById('joke');
@@ -21,7 +24,17 @@ async function fetchJoke(): Promise<void> {
 
     const jokeData = await response.json();
     const joke = jokeData.joke;
-    jokeElement.textContent = `"${joke}"`; //joke;
+    const currentTime = new Date().toISOString();
+
+    reportAcudits.push({
+      joke: joke,
+      score: jokeScore,
+      currentTime: currentTime,
+    });
+
+    indexArray = reportAcudits.length - 1;
+    jokeElement.textContent = `"${joke}"`;
+
   } catch (error) {
     console.error('Failed to fetch joke:', error);
     jokeElement.textContent = 'Failed to load joke.';
@@ -31,6 +44,26 @@ async function fetchJoke(): Promise<void> {
 // Fetch and display the joke when the page loads
 document.addEventListener('DOMContentLoaded', fetchJoke);
 
+console.log(reportAcudits);
+
 function nextAcudit() {
   fetchJoke();
+  console.log(reportAcudits);
 }
+
+//Get the joke score from the user
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelectorAll('.btn-group .btn');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+      const target = event.target as HTMLButtonElement;
+      const buttonValue = target.getAttribute('data-value');
+      jokeScore = buttonValue;
+      reportAcudits[indexArray].score = parseInt(jokeScore);
+      //reset variable
+      jokeScore = 0;
+      console.log(reportAcudits);
+    });
+  });
+});
